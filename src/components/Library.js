c59a52d25f5a08d8d1a416152f3490542461c96f
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-09-23 17:35:38 -0500

    finished exercise

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Library extends Component {
@@ -11,9 +13,12 @@ class Library extends Component {
       <section className='library'>
        {
          this.state.albums.map( (album, index) =>
-           <div key={index} >
-           {album.title}
-           </div>
+           <Link to={'/album/${album.slug}'} key={index}>
+             <img src={album.albumCover} alt={album.title} />
+             <div>{album.title}</div>
+             <div>{album.artist}</div>
+             <div>{album.songs.lenth} songs</div>
+           </Link>
        )
        }
       </section>
