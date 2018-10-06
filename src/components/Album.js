80faf588fdfc24bc6fc88869d987fd8affa5f2b5
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-10-06 12:21:32 -0500

    First issue

--- src/components/Album.js
+++ src/components/Album.js
@@ -51,6 +51,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(4, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
      onMouseEnter(index) {
        this.setState({isHovered: index});
 
@@ -105,7 +113,8 @@ class Album extends Component {
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
            handleSongClick={ () => this.handleSongClick(this.state.currentSong)}
-           handlePrevClick={() => this.handlePrevClick()}
+           handlePrevClick={ () => this.handlePrevClick()}
+           handleNextClick={ () => this.handleSongClick(this.state.currentSong)}
           />
       </section>
     );
